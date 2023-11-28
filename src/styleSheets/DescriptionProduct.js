13ab7f1804c14ee09090dcfa1f8ed7fb2fa583d2
hungtqclone <121530023+hungtqclone@users.noreply.github.com>
2023-11-28 18:46:40 +0700

    add description , update assets

--- src/styleSheets/DescriptionProduct.js
+++ src/styleSheets/DescriptionProduct.js
@@ -1,11 +1,31 @@
 import { StyleSheet } from "react-native";
 
 export const DescriptionProductStyles = StyleSheet.create({
+
+    back: {
+        position: 'absolute',
+        margin: 10,
+        marginTop: 15
+    },
     title: {
         textAlign: "center",
         color: "black",
         fontWeight: "500",
         fontSize: 20,
-        marginTop: 10
+        marginTop: 10,
+        flex: 0.8
+    },
+    titleItem: {
+        padding: 10,
+        backgroundColor: "#FFBF17",
+        alignSelf: 'flex-start',
+        color: "black"
+    },
+    inputText: {
+        backgroundColor: "white",
+        width: "100%",
+        height: "65%",
+        textAlignVertical: 'top'
     }
+
 })
